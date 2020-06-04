8da8e286b3a0b4d226ba554f93b8849a767b7398
Roman <elgoog6651@gmail.com>
2020-06-04 16:55:21 +0300

    tutorial part 3

--- mysity/polls/views.py
+++ mysity/polls/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
-
+from django.template import loader
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You`re looking at question {question_id}")
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse(f"You`re voting on quetion {question_id}")
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question':question})
